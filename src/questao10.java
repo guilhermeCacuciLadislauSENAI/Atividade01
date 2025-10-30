c9a0793b5d90b29db6962409a415ab58daadbfcd
Guilherme Caçuci Ladislau <0001155878@senaimgaluno.com.br>
2025-10-30 10:28:33 -0300

    Subindo a atividade 1

---
+++ src/questao10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class questao10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número para ver a tabuada: ");
+        int numero = sc.nextInt();
+
+        System.out.println("=== TABUADA DO " + numero + " ===");
+        for(int i = 0; i < 10; i++){
+            System.out.println(numero + " x " + (i + 1) + " = " + (numero * (i + 1)));
+        }
+    }
+}
