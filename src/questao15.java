fc55e276eedceec2c1eff27c1c292d8521d636ce
Guilherme Caçuci Ladislau <0001155878@senaimgaluno.com.br>
2025-10-30 23:40:07 -0300

    Adicionando a questao 15 e 16

---
+++ src/questao15.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class questao15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int contador = 10;
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+        System.out.println("\n=== TABUADA ===");
+        do{
+            System.out.println(numero + " x " + contador + " = " + (numero * contador));
+            contador --;
+        }while(contador != 0);
+
+    }
+}
---
+++ src/questao16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class questao16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero, par = 0, impar = 0;
+
+        System.out.print("Digite um número: ");
+        numero = sc.nextInt();
+
+        for(int i = 1; i <= numero; i++){
+            if(i % 2 == 0){
+                par++;
+            }else{
+                impar++;
+            }
+        }
+
+        System.out.print("De 1 até " + numero +" possui " + par + " números pares e " + impar + " números ímpares.");
+    }
+}
